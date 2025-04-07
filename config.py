e46c9819382257f6ba6e07d5141575fe44f62429
malik-l0l <154656931+malik-l0l@users.noreply.github.com>
2025-04-07 11:09:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 GROQ_API_KEY = "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"
+# this api key is fake/deleted/unusable ;), visit https://console.groq.com/keys to create your own
 SECRET_KEY = "your_secret_key_here"
